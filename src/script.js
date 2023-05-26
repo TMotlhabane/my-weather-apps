575e930e773f67fcb539b42857a6f9a6236f33a5
TMotlhabane <motlhabanet23@gmail.com>
2023-05-26 15:15:18 +0200

    fixed js forecast and no fake data

--- src/script.js
+++ src/script.js
@@ -10,31 +10,6 @@ let day= days[date.getDay()];
 return `${day}, ${hours}:${minutes}`;
 }
 
-function updateWeather(response){
-    let townId = (response.data.name);
-    let town = document.querySelector(`#town`);
-    town.innerHTML = (`${townId}`);
-
-    getTemperature(response);
-    description(response);
-    getHumidity(response);
-    getWind(response);
-    getPrecipitation(response);
-}; 
-
-function handleButton(){
-    navigator.geolocation.getCurrentPosition(buttonFunction);
-
-}
-
-function buttonFunction(position){ 
-    let lat = (response.coord.lat);
-    let lon = (response.coord.lon);
-    let apiKey = (`540c88386d3856c60e763ebebac0656f`);
-let apiUrl = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
-axios.get(apiUrl).then(updateWeather);
-};
-
 function getTemperature(response) { console.log(response.data);
      celciusTemperature = Math.round(response.data.main.temp);
     let h3 = document.querySelector(`#h3`);
@@ -85,7 +60,7 @@ function getPrecipitation(response){
 
 function getForecast(coordinates){
 console.log(coordinates)
-let apiKey= (`540c88386d3856c60e763ebebac0656f`);
+let apiKey= (`9e0fb79c2f66d0cd0dcf06710976a873`);
 let apiUrl= (`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`);
 
 axios.get(apiUrl).then(displayForecast);
@@ -96,10 +71,10 @@ function formFunction(event){ event.preventDefault();
     let h1 = document.querySelector (`h1`);
     h1.innerHTML =(`${city}`);
 
-    api(city);
+    showCity(city);
 }  ; 
 
-function api (city){
+function showCity(city){
 let apiKey = (`540c88386d3856c60e763ebebac0656f`);
 let apiUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
 
@@ -148,9 +123,9 @@ if (index <6)
 forecastHTML = forecastHTML + `
 <div class= "col-2">
 <div id="forecastDate">
-${formateForecastDay(forecastDay.dt)}
+${formatForecastDay(forecastDay.dt)}
 </div>
-<img src= "https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="" width="42" />
+<img src= "https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42" />
 <div id="forcastTemperatures">
     <span id="maxTemp">${Math.round(forecastDay.temp.max)}°</span><span id = "minTemp">${Math.round(forecastDay.temp.min)}°</span>
 </div>
@@ -168,11 +143,11 @@ forecastHTML=forecastHTML+ `</div>`;
 let form = document.querySelector (`#entryForm`);
 form.addEventListener (`submit` , formFunction);
 
-let button = document.querySelector(`button`);
-button.addEventListener(`click`, handleButton);
 
 let fahrenheitLink = document.querySelector("#fahrenheitLink");
 fahrenheitLink.addEventListener(`click`, showFahrenheitTemperature);
 
 let celciusLink = document.querySelector("#celciusLink");
 celciusLink.addEventListener(`click`, showCelciusTemperature);
+
+showCity(`Meyerton`);
